4c43ed4850dfcd8247417efff527dc8ffd152906
Zhen, Chen <Chen.Zhen@desay-svautomotive.com>
2019-12-02 09:41:10 +0800

    Revert "input的font-family统一"
    This reverts commit 6aee795cff0c38facf4dbfb4ac434a5931b0c6fb.

--- src/views/todo_pc.vue
+++ src/views/todo_pc.vue
@@ -990,7 +990,6 @@ $textColor2: #555;
           width: 90%;
           height: 26px;
           line-height: 26px;
-          font-family: "Microsoft YaHei";
           font-size: 13px;
           letter-spacing: 0.6px;
           background-color: rgba(255, 255, 255, 0.1);
